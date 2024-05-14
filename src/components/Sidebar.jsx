f5d806fb5aec35a3b0aa042421bab86734a2cbbf
millerbermeo <millerbermeo30@gmail.com>
2024-05-14 10:00:42 -0500

    commit sidebar top

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ export default function Sidebar({ children }) {
                         </button>
                     </div>
 
-                    <div className="mt-10 pt-4 border-t-[1px] border-white border-opacity-50">
+                    <div className=" mt-5 2xl:mt-10 pt-4 border-t-[1px] border-white border-opacity-50">
                         <span className="ml-3 h-8 items-center flex">
                             MENU
                         </span>
@@ -37,7 +37,7 @@ export default function Sidebar({ children }) {
                         </SidebarContext.Provider>
                     </div>
 
-                    <div className="border-t flex p-3 mt-[28%] 2xl:mt-[45%] border-white cursor-pointer border-opacity-50">
+                    <div className="border-t flex p-3 mt-[12%]  2xl:mt-[45%] border-white cursor-pointer border-opacity-50">
                         {/* <img src={profile} className="w-10 h-10 rounded-md" /> */}
                         <Power className="ml-2.5" size={20} />
                         <div className={`flex justify-between items-center overflow-hidden cursor-pointer transition-all ${expanded ? "w-48 ml-3" : "w-0"} `}>
